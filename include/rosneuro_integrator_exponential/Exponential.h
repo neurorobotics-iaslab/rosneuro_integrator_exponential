866be22fe1d1482781469e18d5ae00f6884e811c
mirovix <miroljubmihailovic98@gmail.com>
2024-04-19 18:13:11 +0200

    feat: refactor function's and var's name

--- include/rosneuro_integrator_exponential/Exponential.h
+++ include/rosneuro_integrator_exponential/Exponential.h
@@ -5,51 +5,50 @@
 #include <ros/ros.h>
 #include <pluginlib/class_list_macros.h>
 #include <dynamic_reconfigure/server.h>
-
+#include <gtest/gtest_prod.h>
 #include "rosneuro_integrator/GenericIntegrator.h"
 #include "rosneuro_integrator_exponential/ExponentialConfig.h"
 
 
 namespace rosneuro {
 	namespace integrator {
+        using rosneuro_config_exponential = rosneuro_integrator_exponential::ExponentialConfig;
+        using dyncfg_exponential          = dynamic_reconfigure::Server<rosneuro_config_exponential>;
+        class Exponential : public GenericIntegrator {
 
-using rosneuro_config_exponential = rosneuro_integrator_exponential::ExponentialConfig;
-using dyncfg_exponential          = dynamic_reconfigure::Server<rosneuro_config_exponential>;
+            public:
+                Exponential(void);
+                ~Exponential(void);
 
-class Exponential : public GenericIntegrator {
+                bool configure(void);
+                Eigen::VectorXf apply(const Eigen::VectorXf& input);
+                bool reset(void);
+                void setAlpha(float value);
+                void setRejection(float value);
 
-	public:
-		Exponential(void);
-		~Exponential(void);
+            private:
+                Eigen::VectorXf uniformVector(float value);
+                void onRequestReconfigure(rosneuro_config_exponential &config, uint32_t level);
 
-		bool configure(void);
-		Eigen::VectorXf apply(const Eigen::VectorXf& input);
-		bool reset(void);
-		void setalpha(float value);
-		void setrejection(float value);
+                ros::NodeHandle p_nh_;
+                float alpha_, rejection_;
+                bool has_rejection_;
+                const float alpha_default_ = 0.98f;
+                Eigen::Vector2f data_;
 
+                dyncfg_exponential reconfigure_srv_;
+                dyncfg_exponential::CallbackType reconfigure_callback_type_;
 
-	private:
-		Eigen::VectorXf uniform_vector(float value);
-		void on_request_reconfigure(rosneuro_config_exponential &config, uint32_t level);
-
-	private:
-		ros::NodeHandle p_nh_;
-		float alpha_;
-		float rejection_;
-		bool has_rejection_;
-		const float alpha_default_ = 0.98f;
-		Eigen::Vector2f data_;
-
-		dyncfg_exponential recfg_srv_;
-  		dyncfg_exponential::CallbackType recfg_callback_type_;
-		
-
-
-};
-
-PLUGINLIB_EXPORT_CLASS(rosneuro::integrator::Exponential, rosneuro::integrator::GenericIntegrator)
-
+                FRIEND_TEST(TestGenericIntegratorSuite, TestConstructor);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestConfigure);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestApplyWarn);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestApplyNoRejection);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestApplyRejection);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestReset);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestSetAlpha);
+                FRIEND_TEST(TestGenericIntegratorSuite, TestSetRejection);
+        };
+        PLUGINLIB_EXPORT_CLASS(rosneuro::integrator::Exponential, rosneuro::integrator::GenericIntegrator);
 	}
 }
 
--- src/Exponential.cpp
+++ src/Exponential.cpp
@@ -2,96 +2,83 @@
 
 namespace rosneuro {
 	namespace integrator {
+        Exponential::Exponential(void) : p_nh_("~") {
+            this->setName("exponential");
+            this->data_ = this->uniformVector(0.5f);
+            this->has_rejection_ = true;
+        }
 
-Exponential::Exponential(void) : p_nh_("~") {
-	this->setname("exponential");
-	this->data_ = this->uniform_vector(0.5f);
-	this->has_rejection_ = true;
-}
+        Exponential::~Exponential(void) {
+        }
 
-Exponential::~Exponential(void) {
-}
+        bool Exponential::configure(void) {
+            float alpha, rejection;
+            this->p_nh_.param<float>("alpha", alpha, this->alpha_default_);
+            this->setAlpha(alpha);
 
-bool Exponential::configure(void) {
-	
-	float alpha, rejection;
-	
-	this->p_nh_.param<float>("alpha", alpha, this->alpha_default_);
-	this->setalpha(alpha);
-	
-	if(this->p_nh_.param("rejection", rejection) == false) {
-		this->has_rejection_ = false;
-	} else {
-		this->setrejection(rejection);
-	}
+            if(!this->p_nh_.param("rejection", rejection))
+                this->has_rejection_ = false;
+            else
+                this->setRejection(rejection);
 
-	// Bind dynamic reconfigure callback
-	this->recfg_callback_type_ = boost::bind(&Exponential::on_request_reconfigure, this, _1, _2);
-	this->recfg_srv_.setCallback(this->recfg_callback_type_);
+            this->reconfigure_callback_type_ = boost::bind(&Exponential::onRequestReconfigure, this, _1, _2);
+            this->reconfigure_srv_.setCallback(this->reconfigure_callback_type_);
 
-	return true;
-}
+            return true;
+        }
 
-Eigen::VectorXf Exponential::apply(const Eigen::VectorXf& input) {
+        Eigen::VectorXf Exponential::apply(const Eigen::VectorXf& input) {
+            if(input.size() != 2) {
+                ROS_WARN("[%s] Input size is not 2: only 2-class input is allowed", this->name().c_str());
+                return this->data_;
+            }
 
-	if(input.size() != 2) {
-		ROS_WARN("[%s] Input size is not 2: only 2-class input is allowed", this->name().c_str()); 
-		return this->data_;
-	}
+            if(this->has_rejection_ && input.maxCoeff() <= this->rejection_)
+                return this->data_;
 
-	if(this->has_rejection_ == true && input.maxCoeff() <= this->rejection_)
-		return this->data_;
+            this->data_ = this->data_ * this->alpha_ + input * (1 - this->alpha_);
+            return this->data_;
+        }
 
-	this->data_ = this->data_ * this->alpha_ + input * (1 - this->alpha_);
+        bool Exponential::reset(void) {
+            this->data_ = this->uniformVector(0.5f);
+            return true;
+        }
 
-	return this->data_;
-}
+        Eigen::VectorXf Exponential::uniformVector(float value) {
+            return Eigen::Vector2f::Constant(value);
+        }
 
-bool Exponential::reset(void) {
-	this->data_ = this->uniform_vector(0.5f);
-	return true;
-}
+        void Exponential::setAlpha(float value) {
+            if(value < 0.0f | value > 1.0f) {
+                this->alpha_ = this->alpha_default_;
+                ROS_INFO("[%s] Alpha value is not legal (alpha=%f). Alpha set to the default value (alpha=%f)",
+                         this->name().c_str(), value, this->alpha_);
+            } else {
+                this->alpha_ = value;
+                ROS_INFO("[%s] Alpha set to %f", this->name().c_str(), this->alpha_);
+            }
+        }
 
-Eigen::VectorXf Exponential::uniform_vector(float value) {
-	return Eigen::Vector2f::Constant(value);
-}
+        void Exponential::setRejection(float value) {
+            if(value < 0.5f | value > 1.0f) {
+                this->has_rejection_ = false;
+                this->alpha_ = this->alpha_default_;
+                ROS_INFO("[%s] Rejection value is not legal (rejection=%f)", this->name().c_str(), value);
+            } else {
+                this->rejection_ = value;
+                ROS_INFO("[%s] Rejection set to %f", this->name().c_str(), this->rejection_);
+            }
+        }
 
-void Exponential::setalpha(float value) {
+        void Exponential::onRequestReconfigure(rosneuro_config_exponential &config, uint32_t level) {
+            if( std::fabs(config.alpha - this->alpha_) > 0.00001) {
+                this->setAlpha(config.alpha);
+            }
 
-	if(value < 0.0f | value > 1.0f) {
-		this->alpha_ = this->alpha_default_;
-		ROS_INFO("[%s] Alpha value is not legal (alpha=%f). Alpha set to the default value (alpha=%f)",
-				 this->name().c_str(), value, this->alpha_);
-	} else {
-		this->alpha_ = value;
-		ROS_INFO("[%s] Alpha set to %f", this->name().c_str(), this->alpha_);
-	}
-}
-
-void Exponential::setrejection(float value) {
-
-	if(value < 0.5f | value > 1.0f) {
-		this->has_rejection_ = false;
-		this->alpha_ = this->alpha_default_;
-		ROS_INFO("[%s] Rejection value is not legal (rejection=%f)",
-				 this->name().c_str(), value);
-	} else {
-		this->rejection_ = value;
-		ROS_INFO("[%s] Rejection set to %f", this->name().c_str(), this->rejection_);
-	}
-}
-
-
-void Exponential::on_request_reconfigure(rosneuro_config_exponential &config, uint32_t level) {
-
-	if( std::fabs(config.alpha - this->alpha_) > 0.00001) {
-		this->setalpha(config.alpha);
-	}
-	
-	if( std::fabs(config.rejection - this->rejection_) > 0.00001) {
-		this->setrejection(config.rejection);
-	}
-}
-
+            if( std::fabs(config.rejection - this->rejection_) > 0.00001) {
+                this->setRejection(config.rejection);
+            }
+        }
 	}
 }
